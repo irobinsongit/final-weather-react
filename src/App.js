c81e831526356ce4bc1b5160268d0f4ece66c4a1
irobinsongit <isobellepandas@gmail.com>
2023-06-07 12:16:34 +0100

    Added the GitHub repo link.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        <a
+          href="https://github.com/irobinsongit/final-weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          This project was created by Izzy and is open sourced.
+        </a>
+      </footer>
     </div>
   );
 }
