3ee5681165f681723e0d10e2bd2b804840385585
irobinsongit <isobellepandas@gmail.com>
2023-06-07 12:25:24 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <a
-          href="https://github.com/irobinsongit/final-weather-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          This project was created by Izzy and is open sourced.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <a
+            href="https://github.com/irobinsongit/final-weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            This project was created by Izzy and is open sourced.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
