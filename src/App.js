99da2a5f6130e19ebe394294849265b218462826
irobinsongit <isobellepandas@gmail.com>
2023-06-17 21:19:08 +0100

    Committed final changes.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York" />
+        <Weather defaultCity="Tokyo" />
         <footer>
           <a
             href="https://github.com/irobinsongit/final-weather-react"
